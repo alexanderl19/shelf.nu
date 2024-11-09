18d1425191847bb9cc2258e372383ebac7e676a0
Alexander Liu <a@alexanderliu.com>
2024-11-09 14:31:28 -0800

    style: format tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,15 +8,15 @@ export default {
   darkMode: "class",
   theme: {
     borderRadius: {
-      "none": "0",
-      "sm": "0.125rem",
+      none: "0",
+      sm: "0.125rem",
       DEFAULT: "0.5rem",
-      "md": "0.375rem",
-      "lg": "0.5rem",
-      "xl": "0.75rem",
+      md: "0.375rem",
+      lg: "0.5rem",
+      xl: "0.75rem",
       "2xl": "1rem",
       "3xl": "1.5rem",
-      "full": "9999px",
+      full: "9999px",
     },
     extend: {
       fontSize: {
@@ -234,14 +234,10 @@ export default {
 
       boxShadow: {
         DEFAULT: "0px 1px 2px rgba(16, 24, 40, 0.05)",
-        sm:
-          "0px 1px 3px rgba(16, 24, 40, 0.1), 0px 1px 2px rgba(16, 24, 40, 0.06)",
-        md:
-          "0px 4px 8px -2px rgba(16, 24, 40, 0.1), 0px 2px 4px -2px rgba(16, 24, 40, 0.06)",
-        lg:
-          "0px 12px 16px -4px rgba(16, 24, 40, 0.08), 0px 4px 6px -2px rgba(16, 24, 40, 0.03)",
-        xl:
-          "0px 20px 24px -4px rgba(16, 24, 40, 0.08), 0px 8px 8px -4px rgba(16, 24, 40, 0.03)",
+        sm: "0px 1px 3px rgba(16, 24, 40, 0.1), 0px 1px 2px rgba(16, 24, 40, 0.06)",
+        md: "0px 4px 8px -2px rgba(16, 24, 40, 0.1), 0px 2px 4px -2px rgba(16, 24, 40, 0.06)",
+        lg: "0px 12px 16px -4px rgba(16, 24, 40, 0.08), 0px 4px 6px -2px rgba(16, 24, 40, 0.03)",
+        xl: "0px 20px 24px -4px rgba(16, 24, 40, 0.08), 0px 8px 8px -4px rgba(16, 24, 40, 0.03)",
         "2xl": "0px 24px 48px -12px rgba(16, 24, 40, 0.18)",
         "3xl": "0px 32px 64px -12px rgba(16, 24, 40, 0.14)",
         "tremor-input": "0 1px 2px 0 rgb(0 0 0 / 0.05)",
